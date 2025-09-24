01826c2e835032beb5c3db6901d142c4bca26c25
Muris Poprzenovic <muckyyy@gmail.com>
2025-09-24 21:34:28 +0200

    Reverb config change

--- config/reverb.php
+++ config/reverb.php
@@ -86,7 +86,7 @@ return [
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
                 'max_connections' => env('REVERB_APP_MAX_CONNECTIONS'),
-                'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
+                'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 30_000),
             ],
         ],
 
