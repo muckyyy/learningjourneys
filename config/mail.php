792d7ae2fa4101e75c14c95d3d7da2b60eb9fdd9
Muris Poprzenovic <muckyyy@gmail.com>
2026-03-28 14:46:06 +0100

    proper from email

--- config/mail.php
+++ config/mail.php
@@ -93,7 +93,11 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => str_replace(
+            '${APP_NAME}',
+            env('APP_NAME', 'Example'),
+            env('MAIL_FROM_NAME', env('APP_NAME', 'Example'))
+        ),
     ],
 
     /*
